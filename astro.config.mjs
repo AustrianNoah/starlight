a29a3d025e59be71836e835f417d8a37e86fe67e
Noah <151822733+AustrianNoah@users.noreply.github.com>
2025-09-26 07:28:41 +0200

    schmarotzer

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,9 +22,7 @@ export default defineConfig({
 				},
 				{
 					label: "Home",
-					items: [
-						{ label: 'Home', slug: 'home/home '},
-					]
+					autogenerate: { directory: 'home' },
 				},
 			],
 		}),
